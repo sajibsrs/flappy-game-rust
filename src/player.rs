0ed1e2ae05bda0c3e3aa7ea9ad47ee1b2f544642
Sajidur Rahman <sajibsrs@gmail.com>
2022-02-10 05:43:27 +0600

    added player and its methods

---
+++ src/player.rs
@@ -0,0 +1,42 @@
+use crate::prelude::*;
+
+/// Player character of the game.
+pub struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    pub fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    
+    /// Renders the player on the screen.
+    pub fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+
+    /// Moves player down with time and other screen objects moves left from right.
+    pub fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+
+        self.y += self.velocity as i32;
+        self.x += 1;
+
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    /// Moves player upward.
+    pub fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
