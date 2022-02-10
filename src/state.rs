1275d82332eef3e70e0d052f20165a0ba945c398
Sajidur Rahman <sajibsrs@gmail.com>
2022-02-11 02:46:50 +0600

    play and keypress action implemented

--- src/state.rs
+++ src/state.rs
@@ -23,7 +23,24 @@ impl State {
 
     /// Play the game
     pub fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        ctx.cls_bg(BLUE4);
+        self.frame_time += ctx.frame_time_ms;
+
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+
+        self.player.render(ctx);
+        ctx.print_centered(1, "Press SPACE to flap.");
+
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
 
     /// Restart the game
