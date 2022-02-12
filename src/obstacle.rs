41dbc7bfbee3d3c8795c035d0e43c22028906136
Sajidur Rahman <sajibsrs@gmail.com>
2022-02-12 12:57:41 +0600

    added: new method

--- src/obstacle.rs
+++ src/obstacle.rs
@@ -1,7 +1,20 @@
 use crate::prelude::*;
 
+/// Game obstacle
 pub struct Obstacle {
     x: i32,
     gap_y: i32,
     size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+
+        Obstacle { 
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
 }
